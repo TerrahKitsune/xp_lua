10bcdd4b5ba390ee1d8833c2d8e0155bd7ea21b5
Terrahkitsune <emailarbra@gmail.com>
2019-05-06 20:06:37 +0200

    Added stream to nwnx_lua
    also fixed an oops

--- plua.cpp
+++ plua.cpp
@@ -53,7 +53,7 @@ Implementation of Lua Plugin
 PLua::PLua()
 {
 	header = _T(
-		"NWNX Lua Plugin V.0.3.4\n" \
+		"NWNX Lua Plugin V.0.3.5\n" \
 		"(c) 2019 by Robin Karlsson (Terrahkitsune)\n" \
 		"Lua (c) PUC-Rio: https://www.lua.org/ \n"\
 		"visit us at http://www.nwnx.org\n");
@@ -62,7 +62,7 @@ PLua::PLua()
 		"This plugin provides a lua engine and environment.");
 
 	subClass = _T("LUA");
-	version = _T("0.3.4");
+	version = _T("0.3.5");
 	buffer = NULL;
 	Engine = new LuaEngine();
 
